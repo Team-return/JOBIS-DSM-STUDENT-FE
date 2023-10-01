cee049eac7e2efab623bda271e2a79e4c101924d
KANGYONGSU23 <edc20203@gmail.com>
2023-10-02 00:40:59 +0900

    fix :: 코드 리펙토링

--- src/util/regex.ts
+++ src/util/regex.ts
@@ -1,19 +1,15 @@
 export const business_number_regex = (res: string) => {
-  return res.slice(0, 3) + "-" + res.slice(3, 5) + "-" + res.slice(5, 10);
+  return res.replace(/^(\d{2})(\d{3})(\d{5})$/, "$1-$2-$3");
 };
 
 export const pon_number_regex = (number: string | null) => {
-  if (number) {
-    if (number.length === 10)
-      return number.replace(/^(\d{3})(\d{3})(\d{4})$/, $1 - $2 - $3);
-    else if (number.length === 11)
-      return (
-        number.slice(0, 3) +
-        "-" +
-        number.slice(3, 7) +
-        "-" +
-        number.slice(7, 11)
-      );
-  }
-  return null;
+  if (!number) return null;
+
+  if (number.length === 10)
+    return number.replace(/^(\d{3})(\d{3})(\d{4})$/, "$1-$2-$3");
+
+  if (number.length === 11)
+    return number.replace(/^(\d{3})(\d{4})(\d{4})$/, "$1-$2-$3");
+
+  return number;
 };
