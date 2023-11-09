b5c08ed868681d6821b0283bcafe49ed901e44de
KANGYONGSU23 <edc20203@gmail.com>
2023-11-09 22:03:17 +0900

    feat :: 컴포넌트 분리

--- src/components/company/ReviewList.tsx
+++ src/components/company/ReviewList.tsx
@@ -2,9 +2,8 @@
 
 import { getReviewDetails, GetReviewList } from "@/apis/reviews";
 import { getReviewListProps } from "@/apis/reviews/type";
-import { Icon } from "@team-return/design-system";
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import ReviewItem from "./ReviewItem";
 
 export default function ReviewList() {
   const params = useSearchParams();
@@ -27,49 +26,19 @@ export default function ReviewList() {
   return (
     <>
       {reviewList!.reviews.map((item) => (
-        <ReviewContainer {...item} />
+        <ReviewContainers {...item} />
       ))}
     </>
   );
 }
 
-function ReviewContainer({ review_id, writer, date }: getReviewListProps) {
+function ReviewContainers({ review_id, writer, date }: getReviewListProps) {
   const { data: reviewDetails } = getReviewDetails(review_id);
   return (
     <>
-      {reviewDetails?.qna_responses.map((item) => {
-        const [isOpen, setIsOpen] = useState(false);
-        return (
-          <>
-            <div
-              className="w-full px-3 py-5 border-b border-[#e5e5e5] flex justify-between cursor-pointer hover:bg-[#fcfcfc]"
-              onClick={() => setIsOpen((prev) => !prev)}
-            >
-              <p className="leading-6 text-b3 font-r text-prmyBlue03">
-                {item.question}
-              </p>
-              <Icon
-                icon="Chevron"
-                direction={isOpen ? "top" : "bottom"}
-                color="liteBlue"
-              />
-            </div>
-            {isOpen && (
-              <div className="p-4 bg-[#fafafa] w-full flex relative min-h-[100px] pb-10 gap-1">
-                <p className="text-b3 font-r leading-b3 text-[#444444] flex-1 whitespace-pre-line">
-                  {item.answer}
-                </p>
-                <div className="text-caption font-r leading-caption text-[#4F95D4] px-2 py-1 bg-white rounded-[8px] h-[26px]">
-                  {item.area}
-                </div>
-                <p className="text-caption font-r leading-caption text-[#7f7f7f] absolute bottom-4 right-4">
-                  {date} · {writer}
-                </p>
-              </div>
-            )}
-          </>
-        );
-      })}
+      {reviewDetails?.qna_responses.map((item) => (
+        <ReviewItem item={item} writer={writer} date={date} />
+      ))}
     </>
   );
 }
