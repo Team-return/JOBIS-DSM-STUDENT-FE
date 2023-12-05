06c118bda4d7c905f3036ab849ff1e06eba7982a
JJIIIINN <kgb21537@gmail.com>
2023-12-05 14:16:55 +0900

    feat: SEO 최적화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,32 +2,28 @@ import "./globals.css";
 import { Noto_Sans_KR } from "@next/font/google";
 import Header from "@/components/common/Header";
 import Provider from "@/components/Provider";
-import type { Metadata } from "next";
+import SEOConfig from "@/components/SEO";
 
 const notoSans = Noto_Sans_KR({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
-  display: "swap",
+    weight: ["400", "500", "700"],
+    subsets: ["latin"],
+    display: "swap",
 });
 
-export const metadata: Metadata = {
-  title: "JOBIS",
-  description: "취업의 지름길",
-};
-
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html>
-      <body className={notoSans.className}>
-        <Provider>
-          <Header />
-          {children}
-        </Provider>
-      </body>
-    </html>
-  );
+    return (
+        <html>
+            <SEOConfig />
+            <body className={notoSans.className}>
+                <Provider>
+                    <Header />
+                    {children}
+                </Provider>
+            </body>
+        </html>
+    );
 }
---
+++ src/components/SEO.tsx
@@ -0,0 +1,41 @@
+import Head from "next/head";
+
+const SEOConfig = () => {
+    return (
+        <Head>
+            <title>JOBIS</title>
+            <meta name="description" content="취업의 지름길" />
+            <meta
+                name="description"
+                content="대덕소프트웨어마이스터고등학교 채용 관리 서비스"
+            />
+            <meta name="author" content="retrun" />
+            <meta
+                name="keyword"
+                content="JOBIS, jobis, 자비스, 자비츠, DSM, dsm, 대덕소프트웨어마이스터고등학교, 학생, 취업, 채용"
+            />
+
+            <meta name="og:site_name" content="JOBIS" />
+            <meta name="og:title" content="JOBIS" />
+            <meta
+                name="og:description"
+                content="대덕소프트웨어마이스터고등학교 채용 관리 서비스"
+            />
+            <meta
+                name="og:description"
+                content="대덕소프트웨어마이스터고등학교 취업 관리 서비스"
+            />
+            <meta
+                name="og:url"
+                content="https://jobis-student.team-return.com"
+            />
+            <meta
+                name="og:image"
+                content="https://jobis-student.team-return.com/favicon.ico"
+            />
+            <meta name="og:type" content="website" />
+        </Head>
+    );
+};
+
+export default SEOConfig;
