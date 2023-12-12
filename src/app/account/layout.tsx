82ca7dc1d54265b2bba3e2c3010386f794c5d89b
KANGYONGSU23 <edc20203@gmail.com>
2023-12-12 17:33:00 +0900

    design :: body 구조 변경

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -3,6 +3,7 @@ interface PropsType {
 }
 
 export default function Account({ children }: PropsType) {
+  // document.querySelector("body")!.style.backgroundColor = "#f5f5f5";
   return (
     <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
       <div className="w-[400px] block bg-white shadow-elevaiton rounded-[20px] py-[36px] px-[32px] relative">
