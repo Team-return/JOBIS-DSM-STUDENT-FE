238e966270fd81bc440a0a224724f4a68cb7dd94
KANGYONGSU23 <edc20203@gmail.com>
2023-12-14 10:25:39 +0900

    design :: pretendard로 폰트 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,24 @@
-import "./globals.css";
-import { Noto_Sans_KR } from "@next/font/google";
 import Header from "@/components/common/Header";
 import Provider from "@/components/Provider";
 import SEOConfig from "@/components/SEO";
+import localFont from "@next/font/local";
+import "./globals.css";
 
-const notoSans = Noto_Sans_KR({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
-  display: "swap",
+const pretendard = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Pretendard-Bold.woff2",
+      weight: "700",
+    },
+    {
+      path: "../../public/fonts/Pretendard-Medium.woff2",
+      weight: "500",
+    },
+    {
+      path: "../../public/fonts/Pretendard-Regular.woff2",
+      weight: "400",
+    },
+  ],
 });
 
 export default function RootLayout({
@@ -21,7 +32,7 @@ export default function RootLayout({
       <body
         className={
           "w-screen py-[68px] md:px-[17.5vw] sm:px-[7.5vw] min-h-screen " +
-          notoSans.className
+          pretendard.className
         }
       >
         <Provider>
