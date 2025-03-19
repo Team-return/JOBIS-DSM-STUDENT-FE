c1f03346fd5e8e15a310020b1813ee5be8e63c5a
HYUNA <phyunaa0525@gmail.com>
2025-03-19 10:20:19 +0900

    chore :: 취업률 페이지 접근제한 및 학과 이름 수정

--- src/util/getClassName.ts
+++ src/util/getClassName.ts
@@ -7,10 +7,10 @@ export default function getClassName(classId: number) {
       return "소프트웨어 개발 2반";
 
     case 3:
-      return "임베디드 개발과";
+      return "임베디드 소프트웨어과";
 
     case 4:
-      return "인공지능 개발과";
+      return "인공지능 소프트웨어과";
 
     default:
       return "기타";
