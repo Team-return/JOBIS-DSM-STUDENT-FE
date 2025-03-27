f96ed563583c2dfd18f15ddf3f4bb58d804e87a0
HYUNA <phyunaa0525@gmail.com>
2025-03-27 19:46:26 +0900

    chore :: api수정에 따른 연동코드 수정

--- src/components/notice/NoticeListTable.tsx
+++ src/components/notice/NoticeListTable.tsx
@@ -4,17 +4,17 @@ import Link from "next/link"
 import { useGetNoticeListData } from "@/apis/notice";
 
 export default function NoticeListTable () {
-    const {notices} = useGetNoticeListData();
+    const {data} = useGetNoticeListData();
 
     return (
         <>
-            {notices.map(({id, title, created_at}, index) => (
-                <Link key={id} href={`detail/${id}`}>
+            {data?.notices.map((item, index) => (
+                <Link key={item.id} href={`detail/${item.id}`}>
                     <tbody className="flex justify-center items-center border-collapse">
                         <tr className="h-[60px] border-b-[0.5px] border-[#7F7F7F]">
                             <td className="border-none w-[192px] text-[16px] text-[#135C9D] flex justify-center">{index+1}</td>
-                            <td className="border-none w-[735px] text-[16px] flex justify-center">{title}</td>
-                            <td className="border-none w-[192px] text-[16px] flex justify-center">{created_at.substring(0, 10)}</td>
+                            <td className="border-none w-[735px] text-[16px] flex justify-center">{item.title}</td>
+                            <td className="border-none w-[192px] text-[16px] flex justify-center">{item.created_at.substring(0, 10)}</td>
                         </tr>
                     </tbody>
                 </Link>
