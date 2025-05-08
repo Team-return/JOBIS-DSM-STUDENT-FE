f015a267f8b0f8448b15c7a9b8cd07e57fe584ac
seungWoo <ssounsowns@gmail.com>
2025-05-08 10:26:10 +0900

    fix :: 이미지 사진 오류 해결

--- src/components/jobRate/JobCurrentSituation.tsx
+++ src/components/jobRate/JobCurrentSituation.tsx
@@ -30,7 +30,7 @@ export default function JobCurrentSituation() {
                   <img
                     src={`${BASE_URL}/${data.logo_url}`}
                     alt={`${data.company_name} 로고`}
-                    className="w-full h-auto rounded-md"
+                    className="w-[100px] h-[44px]rounded-md"
                   />
                 </div>
               ))}
